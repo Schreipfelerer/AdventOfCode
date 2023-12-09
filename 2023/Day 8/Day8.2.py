375cb4508beddcae7d90ffeb41d87fd4d5c310ea
Schreipfelerer <Thabo.Bals@gmx.de>
2023-12-09 15:27:29 +0100

    Day8.1

--- 2023/Day 8/Day8.2.py
+++ 2023/Day 8/Day8.2.py
@@ -8,12 +8,20 @@ def readInput(use_example=False) -> list:  # Reads the Input cas be set to read 
 
 
 def parseInput(lines):  # parses the input to the desired typ
-    data = []
+    data = [lines[0].strip("\n"), dict()]
+    for line in lines[2:]:
+        data[1][line[0:3]] = (line[7:10], line[12:15])
     return data
 
 
 def solve(data):  # solves the question
-    return None
+    pos = "AAA"
+    steps = 0
+    while pos != "ZZZ":
+        direction = 0 if data[0][steps%len(data[0])] == "L" else 1
+        pos = data[1][pos][direction]
+        steps +=1
+    return steps
 
 
 def main():
