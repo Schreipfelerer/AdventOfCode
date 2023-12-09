de341380baf51f6720e3e99b05a10ddd1b3fcf83
Schreipfelerer <Thabo.Bals@gmx.de>
2023-12-09 15:16:36 +0100

    Day 5.2

--- 2023/Day 5/Day5.2.py
+++ 2023/Day 5/Day5.2.py
@@ -8,7 +8,8 @@ def readInput(use_example=False) -> list:  # Reads the Input cas be set to read 
 
 
 def parseInput(lines):  # parses the input to the desired typ
-    data = [[int(x) for x in lines[0].rstrip("\n").lstrip("seeds: ").split(" ")]]
+    seeds = lines[0].rstrip("\n").lstrip("seeds: ").split(" ")
+    data = [[(int(seeds[i]), int(seeds[i+1])) for i in range(0, len(seeds), 2)]]
     range_map = []
     for line in lines[2:]:
 
@@ -27,14 +28,25 @@ def parseInput(lines):  # parses the input to the desired typ
 def solve(data):  # solves the question
     seeds = data[0]
     for map_con in data[1:]:
-        for seed in seeds:
-            for row in map_con:
-                if seed in range(row[1], row[1]+row[2]+1):
-                    num = seed
+        converted_seeds = []
+        for row in map_con:
+            i = 0
+            while i < len(seeds):
+                seed = seeds[i]
+                if seed is int or row is int:
+                    print("lol")
+                if (row[1] < (seed[0]+seed[1])) and ((row[1]+row[2]) > seed[0]):
+                    overlap = min(seed[0]+seed[1], row[1]+row[2])-max(row[1], seed[0])
+                    converted_seeds.append((max(row[1], seed[0])-row[1]+row[0],overlap))
+                    if row[1] > seed[0]:
+                        seeds.append((seed[0], row[1]-seed[0]))
+                    if row[1]+row[2] < seed[0]+seed[1]:
+                        seeds.append((row[1]+row[2], seed[0]+seed[1]-(row[1]+row[2])))
                     seeds.remove(seed)
-                    seeds.insert(0, num-row[1]+row[0])
-                    break
-    return min(seeds)
+                else:
+                    i+=1
+        seeds = converted_seeds + seeds
+    return min(seeds, key=lambda x: x[0])[0]
 
 
 def main():
