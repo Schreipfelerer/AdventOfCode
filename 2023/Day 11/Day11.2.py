b2cd6f313be3c21d9a1933d4f170f2161c69760c
Thabo.Bals <thabo.bals@tu-dortmund.de>
2023-12-11 16:48:23 +0100

    Day 11

--- 2023/Day 11/Day11.2.py
+++ 2023/Day 11/Day11.2.py
@@ -9,11 +9,39 @@ def readInput(use_example=False) -> list:  # Reads the Input cas be set to read 
 
 def parseInput(lines):  # parses the input to the desired typ
     data = []
-    return data
+    for i, line in enumerate(lines):
+        for y, char in enumerate(line.rstrip("\n")):
+            if char == "#":
+                data.append((i, y))
+    return lines, data
 
 
 def solve(data):  # solves the question
-    return None
+    data, galaxies = data
+    empty_row = []
+    empty_column = []
+
+    for i in range(len(data)):
+        if "#" not in data[i]:
+            empty_row.append(i)
+
+    for i in range(len(data[0])):
+        if all([x[i] == "." for x in data]):
+            empty_column.append(i)
+
+    solution = 0
+    for i, galax_a in enumerate(galaxies):
+        for galax_b in galaxies[i+1:]:
+            distance = abs(galax_a[0]-galax_b[0])+abs(galax_a[1]-galax_b[1])
+            for y in range(min(galax_a[0], galax_b[0]), max(galax_a[0], galax_b[0])):
+                if y in empty_row:
+                    distance += 1000000-1
+            for y in range(min(galax_a[1], galax_b[1]), max(galax_a[1], galax_b[1])):
+                if y in empty_column:
+                    distance += 1000000-1
+            solution += distance
+
+    return solution
 
 
 def main():
