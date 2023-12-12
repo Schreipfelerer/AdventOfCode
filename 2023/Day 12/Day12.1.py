6a181837ac83e14b38a55aec9dff62d2ee4175e7
Schreipfelerer <Thabo.Bals@gmx.de>
2023-12-12 15:45:02 +0100

    Day 12.1

--- 2023/Day 12/Day12.1.py
+++ 2023/Day 12/Day12.1.py
@@ -9,11 +9,52 @@ def readInput(use_example=False) -> list:  # Reads the Input cas be set to read 
 
 def parseInput(lines):  # parses the input to the desired typ
     data = []
+    for line in lines:
+        datapoint = line.rstrip("\n").split(" ")
+        datapoint[1] = [int(x) for x in datapoint[1].split(",")]
+        data.append(datapoint)
     return data
 
 
+def getPossibilities(record):
+    springs, arrangement = record
+
+    arrangement_cp = arrangement[:]
+    is_gap = True
+    i = 0
+    while i < len(springs) and springs[i] != "?" :
+        if springs[i] == ".":
+            is_gap = True
+        else:
+            if not arrangement_cp:
+                return 0
+            if is_gap:
+                if i+arrangement_cp[0] > len(springs):
+                    return 0
+                if "." in springs[i:i+arrangement_cp[0]]:
+                    return 0
+                else:
+                    i += arrangement_cp[0]-1
+                    arrangement_cp = arrangement_cp[1:]
+                    is_gap = False
+            else:
+                return 0
+        i += 1
+
+    if "?" not in springs:
+        return 0 if arrangement_cp else 1
+    possib = 0
+    index = springs.index("?")
+    for replacement in ".#":
+        possib += getPossibilities((springs[:index]+replacement+springs[index+1:], arrangement))
+    return possib
+
+
 def solve(data):  # solves the question
-    return None
+    solution = 0
+    for record in data:
+        solution += getPossibilities(record)
+    return solution
 
 
 def main():
