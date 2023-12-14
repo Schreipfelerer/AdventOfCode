afd1a29d9ae5d97c5ebc7f8be8c79ff712da2cc1
Schreipfelerer <Thabo.Bals@gmx.de>
2023-12-14 17:54:53 +0100

    feat: Day 14Y23

--- 2023/Day 14/Day14.1.py
+++ 2023/Day 14/Day14.1.py
@@ -9,11 +9,30 @@ def readInput(use_example=False) -> list:  # Reads the Input cas be set to read 
 
 def parseInput(lines):  # parses the input to the desired typ
     data = []
+    for row in lines:
+        data_row = []
+        for char in row.rstrip("\n"):
+            data_row.append(char)
+        data.append(data_row)
     return data
 
 
 def solve(data):  # solves the question
-    return None
+    for y in range(len(data[0])):
+        for i in range(1, len(data)):
+            if data[i][y] == "O":
+                empty_rows = 1
+                while data[i - empty_rows][y] == "." and i - empty_rows >= 0:
+                    empty_rows += 1
+
+                if empty_rows > 1:
+                    data[i][y] = "."
+                    data[i - empty_rows + 1][y] = "O"
+
+    load = 0
+    for i in range(len(data)):
+        load += (len(data) - i) * data[i].count("O")
+    return load
 
 
 def main():
