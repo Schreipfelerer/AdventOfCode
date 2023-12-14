5e95cf67547d89bbab1a4041cb79f1f226276255
Schreipfelerer <Thabo.Bals@gmx.de>
2023-12-14 23:26:42 +0100

    fix: Day 8.2Y23

--- 2023/Day 8/Day8.2.py
+++ 2023/Day 8/Day8.2.py
@@ -1,3 +1,6 @@
+import math
+
+
 def readInput(use_example=False) -> list:  # Reads the Input cas be set to read from example.txt
     if use_example:
         with open('example.txt', 'r') as f:
@@ -16,26 +19,41 @@ def parseInput(lines):  # parses the input to the desired typ
 
 def solve(data):  # solves the question
     pos = [x for x in data[1] if x.endswith("A")]
+
     steps = 0
     instruction_length = len(data[0])
     visited_at_start = [[] for _ in pos]
-    while any(not x.endswith("Z") for x in pos):
+    loop_length = [0 for _ in pos]
+    end_indexes = [0 for _ in pos]
+    while True:
         if not steps % instruction_length:
-            if all([pos_i in visited_at_start[i] for i, pos_i in enumerate(pos)]):
-                print([len(pos_i) for pos_i in visited_at_start])
-                print(visited_at_start)
+            if all(loop_length):
+                print(loop_length, end_indexes)
             else:
+                for i, end_index in enumerate(end_indexes):
+                    if not end_indexes[i]:
+                        if pos[i].endswith("Z"):
+                            end_indexes[i] = steps // instruction_length
                 for i, pos_i in enumerate(pos):
-                    if pos_i in visited_at_start[i]:
-                        # print(f"Loop in {i} after {steps//instruction_length} passes.")
-                        pass
-                    else:
-                        visited_at_start[i].append(pos_i)
+                    if not loop_length[i]:
+                        if pos_i in visited_at_start[i]:
+                            loop_length[i] = steps // instruction_length - visited_at_start[i].index(pos_i)
+                            if all(loop_length):
+                                end_indexes = [end_indexes[i] % loop_length[i] for i in range(len(end_indexes))]
+                                while len(loop_length) > 1:
+                                    new_end_index = end_indexes[0] + loop_length[0]
+                                    while new_end_index % loop_length[1] != end_indexes[1]:
+                                        new_end_index += loop_length[0]
+                                    end_indexes[0] = new_end_index
+                                    end_indexes.pop(1)
+                                    loop_length[0] = math.lcm(loop_length[0], loop_length[1])
+                                    loop_length.pop(1)
+                                return end_indexes[0] * instruction_length
+                        else:
+                            visited_at_start[i].append(pos_i)
         direction = 0 if data[0][steps % instruction_length] == "L" else 1
         pos = [data[1][x][direction] for x in pos]
         steps += 1
-
-    return steps
 
 
 def main():
