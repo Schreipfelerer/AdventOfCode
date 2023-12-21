beed7aad81c8ee4848dc52a125c69582902d4a5f
Thabo.Bals <thabo.bals@tu-dortmund.de>
2023-12-21 15:16:20 +0100

    feat: Day18.2

--- 2023/Day 18/Day18.1.py
+++ 2023/Day 18/Day18.1.py
@@ -17,45 +17,29 @@ def parseInput(lines):  # parses the input to the desired typ
 
 
 def solve(data):  # solves the question
-    edges = [(0, 0)]
+    vertices = [(0, 0)]
     for instr in data:
-        for _ in range(instr[1]):
-            offset = (0, 0)
-            if instr[0] == "U":
-                offset = (-1, 0)
-            if instr[0] == "D":
-                offset = (1, 0)
-            if instr[0] == "R":
-                offset = (0, 1)
-            if instr[0] == "L":
-                offset = (0, -1)
-            edges.append((edges[-1][0]+offset[0], edges[-1][1]+offset[1]))
-    assert edges.pop() == (0, 0)
-    min_x = min(edges, key=lambda x: x[0])[0]
-    max_x = max(edges, key=lambda x: x[0])[0]
-    min_y = min(edges, key=lambda x: x[1])[1]
-    max_y = max(edges, key=lambda x: x[1])[1]
-    outside = []
-    queue = [(min_x-1, min_y-1)]
-    while queue:
-        pos = queue.pop(0)
-        if pos in outside:
-            continue
-        if max_x+1 < pos[0] or pos[0] < min_x-1:
-            continue
-        if max_y+1 < pos[1] or pos[1] < min_y-1:
-            continue
-        if pos in edges:
-            continue
-        outside.append(pos)
-        queue.append((pos[0]+1, pos[1]))
-        queue.append((pos[0]-1, pos[1]))
-        queue.append((pos[0], pos[1]+1))
-        queue.append((pos[0], pos[1]-1))
+        offset = (0, 0)
+        if instr[0] == "U":
+            offset = (-1, 0)
+        if instr[0] == "D":
+            offset = (1, 0)
+        if instr[0] == "R":
+            offset = (0, 1)
+        if instr[0] == "L":
+            offset = (0, -1)
+        vertices.append((vertices[-1][0] + instr[1] * offset[0], vertices[-1][1] + instr[1] * offset[1]))
 
-    outside.sort()
+    area = 0
 
-    return (max_x-min_x+3)*(max_y-min_y+3)-len(outside)
+    for i in range(len(vertices) - 1):
+        area += vertices[i][0] * vertices[i + 1][1]
+        area -= vertices[i][1] * vertices[i + 1][0]
+
+    area //= 2
+    area = abs(area)
+    area += sum([x[1] for x in data]) // 2 + 1
+    return area
 
 
 def main():
