c957a32931e075a1b304d9249125ed993e957f63
Schreipfelerer <Schreipfelerer@gmail.com>
2024-12-01 14:29:53 +0100

    renamed folders to match language used

--- 2020/Day 1/Day 1.1.py
+++
@@ -1,9 +0,0 @@
-c = []
-with open("input.txt") as file:
-    for line in file:
-        c.append(int(line))
-
-for each in c:
-    for each2 in c:
-        if each + each2 == 2020:
-            print(str(each) + " * " + str(each2) + " = " + str(each * each2))
