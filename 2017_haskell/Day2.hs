76fc92f836da52b7e6fc634cecf127d9e01ea1e2
Schreipfelerer <Schreipfelerer@gmail.com>
2024-12-02 22:54:41 +0100

    Haskell yeah

---
+++ 2017_haskell/Day2.hs
@@ -0,0 +1,28 @@
+module Main where
+
+type Line = [Int]
+
+loadFile :: String -> IO [Line]
+loadFile fname = do
+  s <- readFile fname
+  case traverse parseLine (lines s) of
+    Nothing -> error "some line failed to parse"
+    Just people -> pure people
+
+
+parseLine :: String -> Maybe Line
+parseLine i = Just $ map read (words i)
+
+solvePart1 :: [Line] -> String
+solvePart1 x = show $ sum $ map (\x -> maximum x - minimum x) x
+
+solvePart2 :: [Line] -> String
+solvePart2 x = show $ sum $ map (\xs -> sum [quot x y | x <- xs, y <- xs, (rem x y) == 0, x /= y]) x
+
+
+main :: IO ()
+main = do
+  lines <- loadFile "input2.txt"
+  putStrLn $ "Part 1: " ++ solvePart1 lines
+  putStrLn $ "Part 2: " ++ solvePart2 lines
+  return ()
