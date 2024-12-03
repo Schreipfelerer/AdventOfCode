05cd80601c5e8931bcca092023cd6dd0e4d03f2d
Schreipfelerer <44339978+Schreipfelerer@users.noreply.github.com>
2024-12-03 15:15:45 +0100

    TODO: Loop detection

--- 2023_python/Day16/Part2.py
+++ 2023_python/Day16/Part2.py
@@ -18,20 +18,22 @@ def parseInput(lines):  # parses the input to the desired typ
 def solve(data):  # solves the question
     biggest = 0
     for x_pos in range(len(data)):
-        tt = getTraveledTrough(data, x_pos, 0, 1)
+        print(f"x: {x_pos} checked / {len(data)}")
+        tt = getTraveledTrough(data, x_pos, 0, 1) # Beams from Left
         current_length = len(tt[0] | tt[1] | tt[2] | tt[3])
         if current_length > biggest:
-            biggest = current_length
-        tt = getTraveledTrough(data, x_pos, len(data[x_pos])-1, 3)
+             biggest = current_length
+        tt = getTraveledTrough(data, x_pos, len(data[x_pos])-1, 3) # Beams from Right
         current_length = len(tt[0] | tt[1] | tt[2] | tt[3])
         if current_length > biggest:
             biggest = current_length
     for y_pos in range(len(data[0])):
-        tt = getTraveledTrough(data, 0, y_pos, 2)
+        print(f"x: {y_pos} checked / {len(data[0])}")
+        tt = getTraveledTrough(data, 0, y_pos, 2) # Beams from Top
         current_length = len(tt[0] | tt[1] | tt[2] | tt[3])
         if current_length > biggest:
             biggest = current_length
-        tt = getTraveledTrough(data, len(data)-1, y_pos, 0)
+        tt = getTraveledTrough(data, len(data)-1, y_pos, 0) # Beams from Bottom
         current_length = len(tt[0] | tt[1] | tt[2] | tt[3])
         if current_length > biggest:
             biggest = current_length
@@ -41,17 +43,31 @@ def solve(data):  # solves the question
 
 memom = {}
 currently_solving = []
+loop = False
 
 def getTraveledTrough(data, x_pos, y_pos, direction):
+    global currently_solving
+    global loop
     traveled_trough = [set(), set(), set(), set()]
     if 0 > x_pos or x_pos >= len(data):
         return traveled_trough
     if 0 > y_pos or y_pos >= len(data[0]):
         return traveled_trough
-    if (x_pos, y_pos, direction) in currently_solving:
-        for i in range(len(currently_solving)-1, currently_solving.index((x_pos, y_pos, direction))-1, -1):
-            traveled_trough[currently_solving[i][2]].add(currently_solving[i][:2])
-        return [t.copy() for t in traveled_trough]
+    if (x_pos, y_pos, direction) in currently_solving: # Loop detection
+        if not loop:
+            loop_index = currently_solving.index((x_pos, y_pos, direction))
+            curren_loop = currently_solving[loop_index:]
+            loop = True
+            currently_solving = currently_solving[:loop_index]
+            r = [t.copy() for t in getTraveledTrough(data, x_pos, y_pos, direction)]
+            currently_solving += curren_loop
+            loop = False
+
+        #for i in range(len(currently_solving)-1, currently_solving.index((x_pos, y_pos, direction))-1, -1):
+        #    traveled_trough[currently_solving[i][2]].add(currently_solving[i][:2])
+            return r
+        else:
+            return traveled_trough
 
     key = str(x_pos)+","+str(y_pos)+","+str(direction)
     if key in memom.keys():
@@ -78,7 +94,7 @@ def getTraveledTrough(data, x_pos, y_pos, direction):
         ntt.append(getTraveledTrough(
             data, x_pos + directions[new_direction][0], y_pos + directions[new_direction][1], new_direction))
     else:
-        for new_direction in (direction + 1, direction - 1):
+        for new_direction in (direction + 1, direction + 3):
             new_direction %= 4
             ntt.append(getTraveledTrough(
                 data,x_pos + directions[new_direction][0], y_pos + directions[new_direction][1], new_direction))
@@ -87,7 +103,8 @@ def getTraveledTrough(data, x_pos, y_pos, direction):
         tt = tt.copy()
         for i in range(4):
             traveled_trough[i] = traveled_trough[i] | tt[i]
-    memom[key] = [t.copy() for t in traveled_trough]
+    if not loop:
+        memom[key] = [t.copy() for t in traveled_trough]
     currently_solving.remove((x_pos, y_pos, direction))
     return traveled_trough
 
