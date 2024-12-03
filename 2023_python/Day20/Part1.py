74d431852b051175761631f1fc56691c3de5a325
Schreipfelerer <44339978+Schreipfelerer@users.noreply.github.com>
2024-12-03 14:09:28 +0100

    Solved 2023Day20P1

--- 2023_python/Day20/Part1.py
+++ 2023_python/Day20/Part1.py
@@ -8,7 +8,7 @@ def readInput(use_example=False) -> list:  # Reads the Input cas be set to read 
 
 
 def parseInput(lines):  # parses the input to the desired typ
-    broadcaster = []
+    broadcast = []
     flipflops = {}
     conjunction = {}
     for line in lines:
@@ -30,7 +30,7 @@ def parseInput(lines):  # parses the input to the desired typ
         for c2_key, c2 in conjunction.items():
             if c_key in c2[1]:
                 c[0][c2_key] = False
-    return (broadcast, flipflops, conjunction)
+    return broadcast, flipflops, conjunction
 
 
 def solve(data):  # solves the question
@@ -38,16 +38,36 @@ def solve(data):  # solves the question
     high_pulses = 0
     broadcast, flipflops, conjunction = data
     for i in range(1000):
-        to_send = [("broadcast", Low)]
+        to_send = [("broadcast", False, "button")]
         while to_send:
-            send = to_send.pop(0)
+            to, signal, fro = to_send.pop(0)
+            if signal:
+                high_pulses += 1
+            else:
+                low_pulses += 1
 
-    
-    return data
+            if to == "broadcast":
+                for s in broadcast:
+                    to_send.append((s, signal, to))
+            elif to in flipflops.keys():
+                if signal:
+                    continue
+                f = flipflops[to]
+                f[0] = not f[0]
+                for s in f[1]:
+                    to_send.append((s, f[0], to))
+            elif to in conjunction.keys():
+                c = conjunction[to]
+                c[0][fro] = signal
+                ss = not all(c[0].values())
+                for s in c[1]:
+                    to_send.append((s, ss, to))
+
+    return low_pulses*high_pulses
 
 
 def main():
-    lines = readInput(True)
+    lines = readInput()
     data = parseInput(lines)
     print(solve(data))
 
