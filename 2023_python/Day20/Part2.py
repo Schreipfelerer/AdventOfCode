48b57ca0f3881a3694734253ef40f3dc13c7ff8c
Schreipfelerer <44339978+Schreipfelerer@users.noreply.github.com>
2024-12-03 15:15:32 +0100

    not happeniung

--- 2023_python/Day20/Part2.py
+++ 2023_python/Day20/Part2.py
@@ -8,12 +8,59 @@ def readInput(use_example=False) -> list:  # Reads the Input cas be set to read 
 
 
 def parseInput(lines):  # parses the input to the desired typ
-    data = []
-    return data
+    broadcast = []
+    flipflops = {}
+    conjunction = {}
+    for line in lines:
+        line = line[:-1]
+        if not line:
+            continue
+        rightside = line.split(" -> ")[1].split(", ")
+        if line.startswith("broadcast"):
+            broadcast = rightside
+        if line.startswith("%"):
+            flipflops[line.split(" -> ")[0][1:]] = [False, rightside]
+        if line.startswith("&"):
+            conjunction[line.split(" -> ")[0][1:]] = [{}, rightside]
+
+    for c_key, c in conjunction.items():
+        for f_key, f in flipflops.items():
+            if c_key in f[1]:
+                c[0][f_key] = False
+        for c2_key, c2 in conjunction.items():
+            if c_key in c2[1]:
+                c[0][c2_key] = False
+    return broadcast, flipflops, conjunction
 
 
 def solve(data):  # solves the question
-    return None
+    button_presses = 0
+    broadcast, flipflops, conjunction = data
+    while True:
+        button_presses += 1
+        to_send = [("broadcast", False, "button")]
+        while to_send:
+            to, signal, fro = to_send.pop(0)
+
+            if to == "rx" and not signal:
+                return button_presses
+
+            if to == "broadcast":
+                for s in broadcast:
+                    to_send.append((s, signal, to))
+            elif to in flipflops.keys():
+                if signal:
+                    continue
+                f = flipflops[to]
+                f[0] = not f[0]
+                for s in f[1]:
+                    to_send.append((s, f[0], to))
+            elif to in conjunction.keys():
+                c = conjunction[to]
+                c[0][fro] = signal
+                ss = not all(c[0].values())
+                for s in c[1]:
+                    to_send.append((s, ss, to))
 
 
 def main():
