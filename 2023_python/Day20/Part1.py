4d0cddc9f6622c2f195219d837c4b863f49d918d
Schreipfelerer <Schreipfelerer@gmail.com>
2024-12-03 13:50:12 +0100

    2023 Day20 try

--- 2023_python/Day20/Part1.py
+++ 2023_python/Day20/Part1.py
@@ -8,16 +8,46 @@ def readInput(use_example=False) -> list:  # Reads the Input cas be set to read 
 
 
 def parseInput(lines):  # parses the input to the desired typ
-    data = []
-    return data
+    broadcaster = []
+    flipflops = {}
+    conjunction = {}
+    for line in lines:
+        line = line[:-1]
+        if not line:
+            continue
+        rightside = line.split(" -> ")[1].split(", ")
+        if line.startswith("broadcast"):
+            broadcast = rightside
+        if line.startswith("%"):
+            flipflops[line.split(" -> ")[0][1:]] = [False, rightside]
+        if line.startswith("&"):
+            conjunction[line.split(" -> ")[0][1:]] = [{}, rightside]
+
+    for c_key, c in conjunction.items():
+        for f_key, f in flipflops.items():
+            if c_key in f[1]:
+                c[0][f_key] = False
+        for c2_key, c2 in conjunction.items():
+            if c_key in c2[1]:
+                c[0][c2_key] = False
+    return (broadcast, flipflops, conjunction)
 
 
 def solve(data):  # solves the question
-    return None
+    low_pulses = 0
+    high_pulses = 0
+    broadcast, flipflops, conjunction = data
+    for i in range(1000):
+        to_send = [("broadcast", Low)]
+        while to_send:
+            send = to_send.pop(0)
+
+    
+    return data
 
 
 def main():
-    lines = readInput()
+    lines = readInput(True)
     data = parseInput(lines)
     print(solve(data))
 
