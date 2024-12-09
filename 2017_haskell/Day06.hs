6286a5c759b6dfa0b1b5b234364b5fa8700d5f4a
Schreipfelerer <Schreipfelerer@gmail.com>
2024-12-09 18:02:58 +0100

    Solved Day6 2017

--- 2017_haskell/Day06.hs
+++ 2017_haskell/Day06.hs
@@ -1,6 +1,9 @@
 #!/usr/bin/env runhaskell
 module Main where
 
+import Data.List (elemIndex)
+import Data.Maybe (fromMaybe)
+
 type Line = [Int]
 
 loadFile :: String -> IO [Line]
@@ -10,24 +13,37 @@ loadFile fname = do
     Nothing -> error "some line failed to parse"
     Just people -> pure people
 
-
 parseLine :: String -> Maybe Line
-parseLine (i:is) = case parseLine is of 
-    Nothing -> Nothing
-    Just rest -> Just $ read [i] : rest
-parseLine i = Just []
+parseLine i = Just $ map read (words i)
 
 solvePart1 :: [Line] -> String
-solvePart1 _ = ""
+solvePart1 (x : _) = show $ loopRedist x []
+
+loopRedist :: Line -> [Line] -> Int
+loopRedist x c =
+  if x `elem` c
+    then 0
+    else 1 + loopRedist (redist x) (x : c)
+
+noLoopRedist :: Line -> [Line] -> Int
+noLoopRedist x c =
+  if x `elem` c
+    then loopRedist x []
+    else noLoopRedist (redist x) (x : c)
+
+redist :: Line -> Line
+redist x =
+  let h = maximum x
+      i = fromMaybe (-1) $ elemIndex h x
+      r = h `mod` length x
+   in zipWith (\y yi -> (if yi == i then 0 else y) + h `quot` length x + fromEnum ((yi - i - 1) `mod` length x < r)) x [0..]
 
 solvePart2 :: [Line] -> String
-solvePart2 _ = ""
-
+solvePart2 (x : _) = show $ noLoopRedist x []
 
 main :: IO ()
 main = do
-  lines <- loadFile "example.txt"
+  lines <- loadFile "input6.txt"
   putStrLn $ "Part 1: " ++ solvePart1 lines
   putStrLn $ "Part 2: " ++ solvePart2 lines
   return ()
-
--- README.md
+++ README.md
@@ -8,6 +8,6 @@
 ![Static Badge](https://img.shields.io/badge/%E2%98%8646%20-%20year?label=Year%202020&color=%2303fc20)  
 ![Static Badge](https://img.shields.io/badge/%E2%98%865%20-%20year?label=Year%202019&color=%2303fc20)  
 ![Static Badge](https://img.shields.io/badge/%E2%98%861%20-%20year?label=Year%202018&color=%2303fc20)  
-![Static Badge](https://img.shields.io/badge/%E2%98%867%20-%20year?label=Year%202017&color=%2303fc20)  
+![Static Badge](https://img.shields.io/badge/%E2%98%8610%20-%20year?label=Year%202017&color=%2303fc20)  
 ![Static Badge](https://img.shields.io/badge/%E2%98%860%20-%20year?label=Year%202016&color=%2303fc20)  
 ![Static Badge](https://img.shields.io/badge/%E2%98%866%20-%20year?label=Year%202015&color=%2303fc20)  
