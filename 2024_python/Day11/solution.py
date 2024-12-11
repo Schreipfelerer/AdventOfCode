f82d3e0be19a48fe71c993eb56e956af3d701fc1
Schreipfelerer <Schreipfelerer@gmail.com>
2024-12-11 14:42:55 +0100

    Refactor

--- 2024_python/Day11/solution.py
+++ 2024_python/Day11/solution.py
@@ -1,10 +1,13 @@
-def readInput(use_example=False) -> list:  # Reads the Input cas be set to read from example.txt
+def read_input(
+    *,
+    use_example=False,
+) -> list:  # Reads the Input cas be set to read from example.txt
     file_loc = "example.txt" if use_example else "input.txt"
-    with open(file_loc, 'r') as f:
+    with open(file_loc) as f:
         return f.read().split("\n")
 
 
-def parseInput(lines):  # parses the input to the desired typ
+def parse_input(lines):  # parses the input to the desired typ
     data = list(map(int, lines[0].split(" ")))
     data_dict = {}
     for d in data:
@@ -12,12 +15,11 @@ def parseInput(lines):  # parses the input to the desired typ
 
     return data_dict
 
-
-def solvePart1(data):  # solves the question
+def solve_part1(data):  # solves the question
     return blink(data, 25)
 
 
-def solvePart2(data):
+def solve_part2(data):
     return blink(data, 75)
 
 
@@ -29,8 +31,8 @@ def blink(data_dict, i):  # solves the question
             if d == 0:
                 dic_add(new_data, 1, data_dict[d])
             elif len(str_d) % 2 == 0:
-                dic_add(new_data, int(str_d[:len(str_d) // 2]), data_dict[d])
-                dic_add(new_data, int(str_d[len(str_d) // 2:]), data_dict[d])
+                dic_add(new_data, int(str_d[: len(str_d) // 2]), data_dict[d])
+                dic_add(new_data, int(str_d[len(str_d) // 2 :]), data_dict[d])
             else:
                 dic_add(new_data, d * 2024, data_dict[d])
         data_dict = new_data
@@ -38,18 +40,18 @@ def blink(data_dict, i):  # solves the question
 
 
 def dic_add(dic, key, to_add):
-    if key in dic.keys():
+    if key in dic:
         dic[key] += to_add
     else:
         dic[key] = to_add
 
 
 def main():
-    lines = readInput()
-    data = parseInput(lines)
-    print(f"Part 1: {solvePart1(data)}")
-    print("")
-    print(f"Part 2: {solvePart2(data)}")
+    lines = read_input()
+    data = parse_input(lines)
+    print(f"Part 1: {solve_part1(data)}")
+    print()
+    print(f"Part 2: {solve_part2(data)}")
 
 
 if __name__ == "__main__":
