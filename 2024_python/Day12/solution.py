6b3b38563779f2672cb8623f53978ca89033841e
Schreipfelerer <44339978+Schreipfelerer@users.noreply.github.com>
2024-12-12 12:33:07 +0100

    solve Day12.2 2024

--- 2024_python/Day12/solution.py
+++ 2024_python/Day12/solution.py
@@ -2,7 +2,7 @@ def read_input(
     *,
     use_example=False,
 ) -> list:  # Reads the Input cas be set to read from example.txt
-    file_loc = "example.txt" if use_example else "input.txt"
+    file_loc = "example2.txt" if use_example else "input.txt"
     with open(file_loc) as f:
         return f.read().split("\n")
 
@@ -16,6 +16,7 @@ def grow_part(lines, x_start, y_start):
     char = lines[y_start][x_start]
     area = 1
     perimeter = 4
+    sides = 4
     next_part = {(x_start+1, y_start), (x_start-1, y_start), (x_start, y_start+1), (x_start, y_start-1)}
     while next_part:
         x, y = next_part.pop()
@@ -26,14 +27,36 @@ def grow_part(lines, x_start, y_start):
         for offset in [(1,0), (0,1), (-1,0), (0,-1)]:
             x_off = x + offset[0]
             y_off = y + offset[1]
+            extra_off = [(1, offset[1]), (-1, offset[1])] if offset[0] == 0 else [(offset[0], 1), (offset[0], -1)]
+            base_off = [(1, 0), (-1, 0)] if offset[0] == 0 else [(0, 1), (0, -1)]
             if (x_off, y_off) in part:
                 perimeter -= 1
+                sides -= 1
+                for i in range(2):
+                    e_x, e_y = extra_off[i]
+                    e_x += x
+                    e_y += y
+                    b_x, b_y = base_off[i]
+                    b_x += x
+                    b_y += y
+                    if (e_x, e_y) in part and (b_x, b_y) not in part:
+                        sides += 1
             else:
                 next_part.add((x_off, y_off))
                 perimeter += 1
+                sides += 1
+                for i in range(2):
+                    e_x, e_y = extra_off[i]
+                    e_x += x
+                    e_y += y
+                    b_x, b_y = base_off[i]
+                    b_x += x
+                    b_y += y
+                    if (e_x, e_y) not in part and (b_x, b_y) in part:
+                        sides -= 1
         area += 1
         part.append((x, y))
-    return part, perimeter, area
+    return part, perimeter, area, sides
 
 
 def solve_part1(data):  # solves the question
@@ -42,7 +65,7 @@ def solve_part1(data):  # solves the question
     for y, line in enumerate(data):
         for x, char in enumerate(line):
             if all([(x, y) not in p for p in parts]):
-                part, perimeter, area = grow_part(data, x, y)
+                part, perimeter, area, _ = grow_part(data, x, y)
                 parts.append(part)
                 price += perimeter*area
     return price
@@ -50,12 +73,20 @@ def solve_part1(data):  # solves the question
 
 
 def solve_part2(data):  # solves the question
-    return len(data)
+    parts = []
+    price = 0
+    for y, line in enumerate(data):
+        for x, char in enumerate(line):
+            if all([(x, y) not in p for p in parts]):
+                part, _, area, sides = grow_part(data, x, y)
+                parts.append(part)
+                price += sides * area
+    return price
 
 
 
 def main():
-    lines = read_input(use_example=True)
+    lines = read_input(use_example=False)
     data = parse_input(lines)
     print(f"Part 1: {solve_part1(data)}")
     print()
