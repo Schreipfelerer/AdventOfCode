a97c184be83ea54a6d5ff43b1dde473deeac892b
Schreipfelerer <Schreipfelerer@gmail.com>
2024-12-15 15:35:22 +0100

    part2?

--- 2024_python/Day15/solution.py
+++ 2024_python/Day15/solution.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 def read_input(
-        *,
-        use_example=False,
+    *,
+    use_example=False,
 ) -> str:  # Reads the Input cas be set to read from example.txt
-    file_loc = "example.txt" if use_example else "input.txt"
+    file_loc = "example2.txt" if use_example else "input.txt"
     with open(file_loc) as f:
         return f.read()
 
@@ -14,15 +14,22 @@ def parse_input(lines):  # parses the input to the desired typ
 
 
 def move(data, x, y, ins):
-    ox, oy = [(1, 0),(0, -1),(-1, 0),(0, 1)][[">", "^", "<", "v"].index(ins)]
-    match data[y+oy][x+ox]:
+    ox, oy = [(1, 0), (0, -1), (-1, 0), (0, 1)][[">", "^", "<", "v"].index(ins)]
+    ox += x
+    oy += y
+    match data[oy][ox]:
         case "#":
             return x, y
         case ".":
-            return x+ox, y+oy
+            data[oy][ox] = data[y][x]
+            data[y][x] = "."
+            return ox, oy
         case "O":
-            return 0, 0
-    return 0, 0
+            if (ox, oy) == move(data, ox, oy, ins):
+                return x, y
+            data[oy][ox] = data[y][x]
+            data[y][x] = "."
+            return ox, oy
 
 
 def solve_part1(data):  # solves the question
@@ -32,7 +39,7 @@ def solve_part1(data):  # solves the question
         if "@" in row:
             x, y = row.index("@"), grid.index(row)
     for ins in instructions:
-        x, y = move(data, x, y, ins)
+        x, y = move(grid, x, y, ins)
 
     gps = 0
     for y, row in enumerate(grid):
@@ -43,7 +50,69 @@ def solve_part1(data):  # solves the question
 
 
 def solve_part2(data):  # solves the question
-    return data
+    grid, instruction = data
+    for y, row in enumerate(grid):
+        grid[y] = list(
+            "".join(row)
+            .replace("#", "##")
+            .replace(".", "..")
+            .replace("O", "[]")
+            .replace("@", "@.")  # noqa: COM812
+        )
+    x, y = 0, 0
+    for row in grid:
+        if "@" in row:
+            x, y = row.index("@"), grid.index(row)
+
+    for ins in instruction:
+        print()
+        print("\n".join(["".join(row) for row in grid]))
+        ox, oy = [(1, 0), (0, -1), (-1, 0), (0, 1)][[">", "^", "<", "v"].index(ins)]
+        to_move = []
+        to_check = [(x, y)]
+        can_move = True
+        while to_check:
+            px, py = to_check.pop(0)
+            if (px, py) in to_move:
+                continue
+            match grid[py+oy][px+ox]:
+                case "#":
+                    can_move = False
+                    break
+                case ".":
+                    to_move.append((px, py))
+                case "[":
+                    if oy:
+                        to_move.append((px, py))
+                        to_check.append((px+1, py+oy))
+                        to_check.append((px, py+oy))
+                    else:
+                        to_move.append((px, py))
+                        to_check.append((px+ox, py))
+                case "]":
+                    if oy:
+                        to_move.append((px, py))
+                        to_check.append((px-1, py+oy))
+                        to_check.append((px, py+oy))
+                    else:
+                        to_move.append((px, py))
+                        to_check.append((px+ox, py))
+
+        if can_move:
+            x += ox
+            y += oy
+            while to_move:
+                px, py = to_move.pop()
+                grid[py+oy][px+ox] = grid[py][px]
+                grid[py][px] = "."
+
+    gps = 0
+    for y, row in enumerate(grid):
+        for x, char in enumerate(row):
+            if char == "[":
+                gps += x + 100 * y
+    return gps
+
 
 
 def main():
