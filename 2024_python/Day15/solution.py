629554395dc834cdfbd55901b24faca750d4c4af
Schreipfelerer <44339978+Schreipfelerer@users.noreply.github.com>
2024-12-15 16:07:56 +0100

    Refactor Day15

--- 2024_python/Day15/solution.py
+++ 2024_python/Day15/solution.py
@@ -15,21 +15,17 @@ def parse_input(lines):  # parses the input to the desired typ
 
 def move(data, x, y, ins):
     ox, oy = [(1, 0), (0, -1), (-1, 0), (0, 1)][[">", "^", "<", "v"].index(ins)]
-    ox += x
-    oy += y
+    ox, oy = x+oy, y+oy
     match data[oy][ox]:
         case "#":
             return x, y
-        case ".":
-            data[oy][ox] = data[y][x]
-            data[y][x] = "."
-            return ox, oy
         case "O":
             if (ox, oy) == move(data, ox, oy, ins):
                 return x, y
-            data[oy][ox] = data[y][x]
-            data[y][x] = "."
-            return ox, oy
+
+    data[oy][ox] = data[y][x]
+    data[y][x] = "."
+    return ox, oy
 
 
 def solve_part1(data):  # solves the question
@@ -41,10 +37,14 @@ def solve_part1(data):  # solves the question
     for ins in instructions:
         x, y = move(grid, x, y, ins)
 
+    return calculate_gps_score(grid)
+
+
+def calculate_gps_score(grid):
     gps = 0
     for y, row in enumerate(grid):
         for x, char in enumerate(row):
-            if char == "O":
+            if char == "O" or char == "[":
                 gps += x + 100 * y
     return gps
 
@@ -65,62 +65,41 @@ def solve_part2(data):  # solves the question
             x, y = row.index("@"), grid.index(row)
 
     for ins in instruction:
-        print()
-        print("\n".join(["".join(row) for row in grid]))
-        ox, oy = [(1, 0), (0, -1), (-1, 0), (0, 1)][[">", "^", "<", "v"].index(ins)]
-        to_move = []
-        to_check = [(x, y)]
-        can_move = True
-        while to_check:
-            px, py = to_check.pop(0)
-            if (px, py) in to_move:
-                continue
-            match grid[py+oy][px+ox]:
-                case "#":
-                    can_move = False
-                    break
-                case ".":
-                    to_move.append((px, py))
-                case "[":
-                    if oy:
-                        to_move.append((px, py))
-                        to_check.append((px+1, py+oy))
-                        to_check.append((px, py+oy))
-                    else:
-                        to_move.append((px, py))
-                        to_check.append((px+ox, py))
-                case "]":
-                    if oy:
-                        to_move.append((px, py))
-                        to_check.append((px-1, py+oy))
-                        to_check.append((px, py+oy))
-                    else:
-                        to_move.append((px, py))
-                        to_check.append((px+ox, py))
+        x, y = move2(grid, ins, x, y)
 
-        if can_move:
-            x += ox
-            y += oy
-            while to_move:
-                px, py = to_move.pop()
-                grid[py+oy][px+ox] = grid[py][px]
-                grid[py][px] = "."
+    return calculate_gps_score(grid)
 
-    gps = 0
-    for y, row in enumerate(grid):
-        for x, char in enumerate(row):
-            if char == "[":
-                gps += x + 100 * y
-    return gps
 
+def move2(grid, ins, x, y):
+    ox, oy = [(1, 0), (0, -1), (-1, 0), (0, 1)][[">", "^", "<", "v"].index(ins)]
+    to_move, to_check = [], [(x, y)]
+    while to_check:
+        px, py = to_check.pop(0)
+        if (px, py) in to_move:
+            continue
+        if (c := grid[py + oy][px + ox]) == "#":
+            break
+        to_move.append((px, py))
+        if c == ".":
+            continue
+        if oy:
+            to_check.append((px + (1 if grid[py + oy][px + ox] == "[" else -1), py + oy))
+        to_check.append((px + ox, py + oy))
+    else:
+        x, y = x+ox, y+oy
+        while to_move:
+            px, py = to_move.pop()
+            grid[py + oy][px + ox] = grid[py][px]
+            grid[py][px] = "."
+
+    return x, y
 
 
 def main():
-    lines = read_input(use_example=True)
-    data = parse_input(lines)
-    print(f"Part 1: {solve_part1(data)}")
+    lines = read_input(use_example=False)
+    print(f"Part 1: {solve_part1(parse_input(lines))}")
     print()
-    print(f"Part 2: {solve_part2(data)}")
+    print(f"Part 2: {solve_part2(parse_input(lines))}")
 
 
 if __name__ == "__main__":
