b9c6ff73c6344958d86322d1e14d7f06a73d0d18
Schreipfelerer <44339978+Schreipfelerer@users.noreply.github.com>
2024-12-16 12:33:19 +0100

    Almost Solved Day16.2

--- 2024_python/Day16/solution.py
+++ 2024_python/Day16/solution.py
@@ -14,9 +14,7 @@ def parse_input(lines):  # parses the input to the desired typ
 
 def solve_part1(data):  # solves the question
     directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-    cur_direction = 0
-    pos = (1, len(data) - 2)
-    queue = [(pos[0], pos[1], cur_direction, 0)]
+    queue = [(1, len(data) - 2, 0, 0)]
     cheapest = {}
     while queue:
         x, y, d, c = queue.pop(0)
@@ -27,18 +25,29 @@ def solve_part1(data):  # solves the question
         if data[y + dy][x + dx] != "#":
             queue.append((x + dx, y + dy, d, c + 1))
         queue += [(x, y, (d + 1) % 4, c + 1000), (x, y, (d - 1) % 4, c + 1000)]
-    return min(cheapest[(len(data) - 2, 1, 0)], cheapest[(len(data) - 2, 1, 3)])
+    return min(cheapest[(len(data) - 2, 1, 0)], cheapest[(len(data) - 2, 1, 3)]), cheapest
 
 
-def solve_part2(data):  # solves the question
-    return data
+def solve_part2(data, cost):  # solves the question
+    directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    best_spots = set()
+    d = 0 if cost[(len(data) - 2, 1, 0)] < cost[(len(data) - 2, 1, 3)] else 3
+    queue = [(len(data) - 2, 1, d)]
+    while queue:
+        x, y, d = queue.pop(0)
+        best_spots.add((x, y))
+        for t in [(x, y, (d+1)%4), (x, y, (d-1)%4), (x-directions[d][0], y-directions[d][1], d)]:
+            if t in cost and cost[t] < cost[(x, y, d)]:
+                queue.append(t)
+    return len(best_spots)
 
 
 def main():
     lines = read_input(use_example=False)
-    print(f"Part 1: {solve_part1(parse_input(lines))}")
+    solve, cheapest = solve_part1(parse_input(lines))
+    print(f"Part 1: {solve}")
     print()
-    print(f"Part 2: {solve_part2(parse_input(lines))}")
+    print(f"Part 2: {solve_part2(parse_input(lines), cheapest)}")
 
 
 if __name__ == "__main__":
