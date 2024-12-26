6b9e2f2680c5203ba84d76f48bc6a863b602cca0
Schreipfelerer <Schreipfelerer@gmail.com>
2024-12-26 18:15:37 +0100

    Day20.2 2024 Solved

--- 2024_python/Day20/solution.py
+++ 2024_python/Day20/solution.py
@@ -74,7 +74,35 @@ def solve_part1(data):  # solves the question
 
 
 def solve_part2(data):  # solves the question
-    return None
+    cheating_threshold = 100
+    end_steps = dijkstra_from_char(data, "E")
+    start_steps = dijkstra_from_char(data, "S")
+    start_loc = index_char(data, "S")
+    normal_steps = end_steps[start_loc[1]][start_loc[0]]
+    cheats = 0
+    for y, row in enumerate(data):
+        for x, c in enumerate(row):
+            if c != "#":
+                for ox in range(-20, 21):
+                    d = 20 - abs(ox)
+                    for oy in range(-d, d + 1):
+                        nx = x + ox
+                        ny = y + oy
+                        if (
+                            0 <= nx < len(data[0])
+                            and 0 <= ny < len(data)
+                            and normal_steps
+                            - (
+                                start_steps[y][x]
+                                + end_steps[ny][nx]
+                                + abs(ox)
+                                + abs(oy)
+                            )
+                            >= cheating_threshold
+                        ):
+                            cheats += 1
+
+    return cheats
 
 
 def main():
