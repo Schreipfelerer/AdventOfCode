29b6c5badae70c76bb9eede463e6611f988c6f76
Schreipfelerer <Schreipfelerer@gmail.com>
2024-12-28 12:41:45 +0100

    Solved Day22 2024

--- 2024_python/Day22/solution.py
+++ 2024_python/Day22/solution.py
@@ -3,7 +3,7 @@ def read_input(
     *,
     use_example=False,
 ) -> list:  # Reads the Input cas be set to read from example.txt
-    file_loc = "example.txt" if use_example else "input.txt"
+    file_loc = "example2.txt" if use_example else "input.txt"
     with open(file_loc) as f:
         return f.read().split("\n")
 
@@ -27,13 +27,38 @@ def solve_part1(data):  # solves the question
     score = 0
     for number in data:
         for _ in range(2000):
-            number = next_secret(number)
+            number = next_secret(number)  # noqa: PLW2901
         score += number
     return score
 
 
 def solve_part2(data):  # solves the question
-    return data
+    changes = []
+    prizes = []
+    for number in data:
+        c = []
+        p = []
+        for _ in range(2000):
+            next_number = next_secret(number)
+            p.append(next_number%10)
+            c.append((next_number%10)-(number%10))
+            number = next_number  # noqa: PLW2901
+        p = p[3:]
+        sliding_window = [(c[i], c[i+1], c[i+2], c[i+3]) for i in range(len(c)-3)]
+        changes.append(sliding_window)
+        prizes.append(p)
+
+    best_bananas = 0
+    d = {xs for x in changes for xs in x}
+    for j, combo in enumerate(d):
+        print(f"starting ({j}/{len(d)})")
+        bananas = 0
+        for i in range(len(data)):
+            index = next((ind for ind, x in enumerate(changes[i]) if x == combo), -1)
+            if index != -1:
+                bananas += prizes[i][index]
+        best_bananas = max(bananas, best_bananas)
+    return best_bananas
 
 
 def main():
