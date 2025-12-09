5df70809ed9fabf8c911e1d735cbd1923965252e
Schreipfelerer <Schreipfelerer@gmail.com>
2025-12-09 20:46:09 +0700

    day 2 but fucking slow

--- 2025_rust/src/day02/mod.rs
+++ 2025_rust/src/day02/mod.rs
@@ -1,61 +1,40 @@
 use crate::utils::read_input_file;
+use fancy_regex::Regex;
 
 pub fn run(input_file: &str, part: Option<&String>) {
     let input = read_input_file("day02", input_file);
 
     match part.map(|s| s.as_str()) {
-        Some("1") => println!("Part 1: {}", part1(&input, 12, 2)),
+        Some("1") => println!("Part 1: {}", part1(&input)),
         Some("2") => println!("Part 2: {}", part2(&input)),
         _ => {
             // run both if no part specified
-            println!("Part 1: {}", part1(&input, 12, 2));
+            println!("Part 1: {}", part1(&input));
             println!("Part 2: {}", part2(&input));
         }
     }
 }
 
-fn part1(input: &str, noun: i64, verb: i64) -> i64 {
-    let mut numbs: Vec<i64> = input
-        .trim()
-        .split(",")
-        .map(|x| x.parse::<i64>().unwrap())
-        .collect();
-    let mut pc = 0;
-
-    numbs[1] = noun;
-    numbs[2] = verb;
-
-    loop {
-        let opcode = numbs[pc];
-        match opcode {
-            1 | 2 => {
-                let a = numbs[pc + 1] as usize;
-                let b = numbs[pc + 2] as usize;
-                let out = numbs[pc + 3] as usize;
-
-                numbs[out] = if opcode == 1 {
-                    numbs[a] + numbs[b]
-                } else {
-                    numbs[a] * numbs[b]
-                };
-
-                pc += 4;
-            }
-            99 => break,
-            _ => unreachable!(),
+fn part1(input: &str) -> i64 {
+    let pattern = Regex::new(r"^(\d+)\1$").unwrap();
+    let mut invalid = 0;
+    for line in input.trim().split(',').map(|x| x.split_once("-").unwrap()){
+        let (from, to) = line;
+        for num in from.parse::<i64>().unwrap()..=to.parse::<i64>().unwrap(){
+            if pattern.is_match(&num.to_string()).unwrap() {invalid += num}
         }
     }
-
-    numbs[0]
+    invalid
 }
 
 fn part2(input: &str) -> i64 {
-    for noun in 0..=99 {
-        for verb in 0..=99 {
-            if part1(input, noun, verb) == 19690720 {
-                return 100 * noun + verb;
-            }
+    let pattern = Regex::new(r"^(\d+)\1+$").unwrap();
+    let mut invalid = 0;
+    for line in input.trim().split(',').map(|x| x.split_once("-").unwrap()){
+        let (from, to) = line;
+        for num in from.parse::<i64>().unwrap()..=to.parse::<i64>().unwrap(){
+            if pattern.is_match(&num.to_string()).unwrap() {invalid += num}
         }
     }
-    -1
+    invalid
 }
