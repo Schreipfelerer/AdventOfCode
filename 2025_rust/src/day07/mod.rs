7043b2c5a6b89cd9efe42d42890badd60ce451aa
Schreipfelerer <Schreipfelerer@gmail.com>
2025-12-10 01:46:22 +0700

    day 7

--- 2025_rust/src/day07/mod.rs
+++ 2025_rust/src/day07/mod.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 use crate::utils::read_input_file;
 
 pub fn run(input_file: &str, part: Option<&String>) {
@@ -15,10 +17,47 @@ pub fn run(input_file: &str, part: Option<&String>) {
 }
 
 fn part1(input: &str) -> i64 {
-    input.lines().count() as i64
+    let mut lines = input.lines();
+    let start = lines.next().unwrap().chars().position(|x| x == 'S').unwrap();
+    let mut splits = 0;
+    let mut beams: HashSet<usize> = HashSet::new();
+    beams.insert(start);
+    for line in lines{
+        let chars: Vec<char> = line.chars().collect();
+        let mut new_beams: HashSet<usize> = HashSet::new(); 
+        for beam in &beams{
+            if chars[*beam] == '^'{
+                new_beams.insert(beam - 1);
+                new_beams.insert(beam + 1);
+                splits += 1
+            }
+            else {
+                new_beams.insert(*beam);
+            }
+        }
+        beams = new_beams;
+    }
+    splits
 }
 
 fn part2(input: &str) -> i64 {
-    input.lines().count() as i64 *2 
+    let mut lines = input.lines();
+    let start = lines.next().unwrap().chars().position(|x| x == 'S').unwrap();
+    let mut beams: HashMap<usize, i64> = HashMap::new();
+    beams.insert(start, 1);
+    for line in lines{
+        let chars: Vec<char> = line.chars().collect();
+        let mut new_beams: HashMap<usize, i64> = HashMap::new(); 
+        for (beam, number) in beams{
+            if chars[beam] == '^'{
+                *new_beams.entry(beam - 1).or_default() += number;
+                *new_beams.entry(beam + 1).or_default() += number;
+            }
+            else {
+                *new_beams.entry(beam).or_default() += number;
+            }
+        }
+        beams = new_beams;
+    }
+    beams.values().sum()
 }
-
