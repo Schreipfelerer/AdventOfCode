96d1b31f0ffc360074568f77a95651e571ac0c4b
Schreipfelerer <Schreipfelerer@gmail.com>
2025-12-11 21:55:13 +0700

    flake

---
+++ 2023_python/flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Simple Python flake for direnv";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.python314
+        ];
+      };
+    };
+}
