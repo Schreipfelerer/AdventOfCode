6c673ef5ef0872d488de7ef80955a2892126ad61
Schreipfelerer <Schreipfelerer@gmail.com>
2025-12-11 23:35:10 +0700

    Heaps for the win

--- 2023_python/Day17/Part1.py
+++ 2023_python/Day17/Part1.py
@@ -1,3 +1,5 @@
+import heapq
+
 def readInput(use_example=False) -> list:  # Reads the Input cas be set to read from example.txt
     if use_example:
         with open('example.txt', 'r') as f:
@@ -15,29 +17,33 @@ def parseInput(lines):  # parses the input to the desired typ
 
 
 def solve(data):  # solves the question
-    cheapest = [[2**31 - 1 for _ in data[0]]for _ in data]
-    to_visit = [(0, 0, 0, 0)]
+    to_visit = [(0, (0, 0, 0)), (0, (0, 0, 3))]
     directions = ((-1, 0), (0, 1), (1, 0), (0, -1))
+    visited = set()
+    height = len(data)
+    width = len(data[0])
+    end = (height-1, width-1)
     while to_visit:
-        c = to_visit.pop(0)
-        if c[2] >= cheapest[c[0]][c[1]]:
+        cost, c = heapq.heappop(to_visit)
+        if c in visited:
             continue
-        cheapest[c[0]][c[1]] = c[2]
+        visited.add(c)
+        if (c[0], c[1]) == end:
+            return cost
         for i, direction in enumerate(directions):
+            if i == c[2] or i == (c[2]+2)%4:
+                continue
+            new_cost = cost
             for y in range(1,4):
                 new_x = c[0]+direction[0]*y
                 new_y = c[1]+direction[1]*y
-                if new_x < 0 or new_x >= len(data):
-                    continue
-                if new_y < 0 or new_y >= len(data[0]):
-                    continue
-                new_cost = c[2]
-                for yy in range(1, y+1):
-                    new_cost +=data[c[0]+direction[0]*yy][c[1]+direction[1]*yy]
-                if i != c[3]:
-                    to_visit.append((new_x, new_y, new_cost, i))
-        # to_visit.sort(key=lambda x: x[2])
-    return cheapest[-1][-1]
+                if new_x < 0 or new_x >= height:
+                    break
+                if new_y < 0 or new_y >= width:
+                    break
+                new_cost += data[new_x][new_y]
+                heapq.heappush(to_visit, (new_cost, (new_x, new_y, i)))
+    return 0
 
 
 def main():
--- 2023_python/Day17/Part2.py
+++ 2023_python/Day17/Part2.py
@@ -1,3 +1,5 @@
+import heapq
+
 def readInput(use_example=False) -> list:  # Reads the Input cas be set to read from example.txt
     if use_example:
         with open('example.txt', 'r') as f:
@@ -9,11 +11,40 @@ def readInput(use_example=False) -> list:  # Reads the Input cas be set to read 
 
 def parseInput(lines):  # parses the input to the desired typ
     data = []
+    for line in lines:
+        data.append([int(x) for x in line.rstrip("\n")])
     return data
 
 
 def solve(data):  # solves the question
-    return None
+    to_visit = [(0, (0, 0, 0)), (0, (0, 0, 3))]
+    directions = ((-1, 0), (0, 1), (1, 0), (0, -1))
+    visited = set()
+    height = len(data)
+    width = len(data[0])
+    end = (height-1, width-1)
+    while to_visit:
+        cost, c = heapq.heappop(to_visit)
+        if c in visited:
+            continue
+        visited.add(c)
+        if (c[0], c[1]) == end:
+            return cost
+        for i, direction in enumerate(directions):
+            if i == c[2] or i == (c[2]+2)%4:
+                continue
+            new_cost = cost
+            for y in range(1,11):
+                new_x = c[0]+direction[0]*y
+                new_y = c[1]+direction[1]*y
+                if new_x < 0 or new_x >= height:
+                    break
+                if new_y < 0 or new_y >= width:
+                    break
+                new_cost += data[new_x][new_y]
+                if y > 3:
+                    heapq.heappush(to_visit, (new_cost, (new_x, new_y, i)))
+    return 0
 
 
 def main():
