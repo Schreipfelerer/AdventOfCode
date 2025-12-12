6da20b6a5907bb0f9ba6df696588ab81e8876468
Schreipfelerer <Schreipfelerer@gmail.com>
2025-12-12 13:10:00 +0700

    no way that works

--- 2025_rust/src/day12/mod.rs
+++ 2025_rust/src/day12/mod.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 use crate::utils::read_input_file;
 
 pub fn run(input_file: &str, part: Option<&String>) {
@@ -15,10 +17,36 @@ pub fn run(input_file: &str, part: Option<&String>) {
 }
 
 fn part1(input: &str) -> i64 {
-    input.lines().count() as i64
+    let mut raw = input.trim_end().split("\n\n").collect_vec();
+    let spaces = raw
+        .pop()
+        .unwrap()
+        .split("\n")
+        .map(|l| l.split_once(": ").unwrap())
+        .map(|i| {
+            (
+                i.0.split_once("x").unwrap(),
+                i.1.split(" ").map(|n| n.parse::<i64>().unwrap()).collect_vec(),
+            )
+        })
+        .collect_vec();
+    let shapes = raw
+        .iter()
+        .map(|f| f.chars().filter(|c| *c == '#').count() as i64)
+        .collect_vec();
+    
+    //naive?
+    let mut fitting_spaces = 0;
+    for ((x, y), requirements) in spaces{
+        let room = x.parse::<i64>().unwrap() * y.parse::<i64>().unwrap();
+        let needed = requirements.iter().enumerate().map(|(i, n)| *n*shapes[i]).sum();
+        if room >= needed{
+            fitting_spaces += 1
+        }
+    }
+    fitting_spaces
 }
 
-fn part2(input: &str) -> i64 {
-    input.lines().count() as i64 *2 
+fn part2(_input: &str) -> i64 {
+    80085
 }
-
